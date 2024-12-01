2b5566cc4ba36ef4e4f950857182c2a3beedf9ef
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-12-02 00:03:49 +0800

    chore(fish): change default editor to neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx EDITOR lvim
+set -gx EDITOR nvim
 
 # starship init fish | source
 zoxide init fish | source

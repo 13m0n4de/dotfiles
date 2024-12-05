b0188cc1fa1837bb67731a38a7c18a2f96297e3b
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-12-05 10:07:45 +0800

    feat(fish): add command alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,24 @@
+# Interactive shell only commands
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Common shortcuts
+    alias nv="nvim"
+    alias pyc="proxychains4 -q"
+
+    # Replace ls commands with eza
+    alias ls="eza --icons"
+    alias ll="eza --icons --long --git"
+    alias la="eza --icons --long --git --all"
+    alias lt="eza --icons --tree"
+    alias l.="eza --icons --grid --all --no-filesize --no-user --no-time | rg '^\.'"
+    alias lg="eza --icons --git --git-repos --long --header"
+
+    # Safe file operations with verbose output
+    alias rm="rm -I -v"
+    alias cp="cp -I -v"
+    alias mv="mv -I -v"
 end
 
+# Environment variables
 set -gx EDITOR nvim
 
 # starship init fish | source
